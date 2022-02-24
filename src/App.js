92e48c959b4c41ecb803ddd0061927fdb0c43cfd
glazko-pol96 <57094984+glazko-pol96@users.noreply.github.com>
2022-02-24 13:54:59 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="elem-animate" id="elem1" />
+      <div className="elem-animate" id="elem2" />
+      <div className="elem-animate" id="elem3" />
+      <div className="elem-animate" id="elem4" />
+      <div className="elem-animate" id="elem5" />
+      <div className="elem-animate" id="elem6" />
+      <div className="elem-animate" id="elem7" />
+      <div className="elem-animate" id="elem8" />
+      <div className="elem-animate" id="elem9" />
+      <div className="elem-animate" id="elem10" />
+    </div>
+  );
+}
+
+export default App;
